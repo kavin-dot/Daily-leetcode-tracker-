bab211369a19a68165009f04789bc1f31e430fd9
kavin s <kavinsp06@gmail.com>
2026-07-09 09:20:30 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:20:29 AM
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res= new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%15==0){
+                res.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                res.add("Fizz");
+            }
+            else if(i%5==0){
+                res.add("Buzz");
+            }
+            else{
+               res.add(String.valueOf(i));
+            }
+        }
+        return res;
+    }
+}
